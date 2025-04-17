235f7e7a8aae765246b72342f52d22d5c3e65174
pradeep-kalyan <pradeepkalyan1275@gmail.com>
2025-04-17 22:41:22 +0530

    ic

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors()); // Allow all origins by default
 connectDB();
 console.log("mongo connected successfully");
-cron.schedule("0 18 * * *", weatherTask); // 1:30 AM UTC = 7:00 AM IST
+cron.schedule("15 5 * * *", weatherTask); // 1:30 AM UTC = 7:00 AM IST
 
 app.use("/api/user/", userRoutes);
 
