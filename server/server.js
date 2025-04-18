7b086c8695b9af92463fe8dcf2c4852fdd3e1669
pradeep-kalyan <pradeepkalyan1275@gmail.com>
2025-04-18 11:41:55 +0530

    ic

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors()); // Allow all origins by default
 connectDB();
 console.log("mongo connected successfully");
-cron.schedule("40 11 * * *", weatherTask, {
+cron.schedule("0 7 * * *", weatherTask, {
   timezone: "Asia/Kolkata",
 });
 console.log("Cron job scheduled to run at 7:00 AM IST every day");
